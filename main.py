91b9c15f2977d47206e1181ff02a92a6006a9474
hsniemi <hennasniemi@gmail.com>
2022-11-08 16:24:57 +0200

    Added Kivy project

---
+++ main.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+
+
+class GridLayoutExample(GridLayout):
+    pass
+
+
+class BoxLayoutExample(BoxLayout):
+    pass
+
+
+class ObjectDetectionApp(App):
+    pass
+
+
+ObjectDetectionApp().run()
