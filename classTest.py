d6bd30f60d930825006e7e9d10a62d9357ee2a5b
Aki Peltokangas <v0peak00@students.oamk.fi>
2022-12-12 09:32:19 +0200

    Testing as class based

---
+++ classTest.py
@@ -0,0 +1,56 @@
+import cv2
+import utils
+
+class Measurement:
+    imgHeightPrefix = 720
+    scaleF = 2
+
+    def __init__(self, widthM, heightM, addressM):
+        self.widthM = widthM
+        self.heightM = heightM
+        self.addressM = addressM
+        print("hello")
+        
+        
+    def capture(self):
+        cap = cv2.VideoCapture(self.addressM)
+        print("hello")
+        while(True):
+            ret, frame = cap.read()
+            out = cv2.transpose(frame)
+            out = cv2.flip(out,flipCode=1)
+            imgResized = utils.resize(out,width=600,height=self.imgHeightPrefix)
+            cv2.imshow('frame',imgResized)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
+
+            imgC, contours = utils.getContours(imgResized)
+            if len(contours) != 0:
+                print("Contours found: %i" % len(contours))
+                biggestContour = contours[0][2]
+                imgWarp = utils.warpImg(imgC,biggestContour,utils.widthP,utils.heightP)
+                imgContours2, contours2 = utils.getContours(imgWarp,
+                    minArea=2000,
+                    cannyT=[50,50])
+                if len(imgContours2) != 0:
+                    for cnt in contours2:
+                        cv2.polylines(imgContours2,[cnt[2]],True,(0,255,0),2)
+                        nPoints = utils.assignPoints(cnt[2],2)
+                        nW = round((utils.findDis(nPoints[0][0]/utils.scaleF,nPoints[1][0]/utils.scaleF)/11),1)
+                        nH = round((utils.findDis(nPoints[0][0]/utils.scaleF,nPoints[2][0]/utils.scaleF)/11),1)
+                        cv2.arrowedLine(imgContours2, (nPoints[0][0][0], nPoints[0][0][1]), (nPoints[1][0][0], nPoints[1][0][1]),
+                                        (255, 0, 255), 3, 8, 0, 0.05)
+                        cv2.arrowedLine(imgContours2, (nPoints[0][0][0], nPoints[0][0][1]), (nPoints[2][0][0], nPoints[2][0][1]),
+                                        (255, 0, 255), 3, 8, 0, 0.05)
+                        x, y, w, h = cnt[3]
+                        cv2.putText(imgContours2, '{}cm'.format(nW), (x + 30, y - 10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5,
+                                    (255, 0, 255), 2)
+                        cv2.putText(imgContours2, '{}cm'.format(nH), (x - 70, y + h // 2), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5,
+                                    (255, 0, 255), 2)
+                        cv2.imshow('A4', imgContours2)
+                else:
+                    break 
+                
+            #print("Contours found: %i" % len(contours2))
+        cv2.destroyAllWindows()
