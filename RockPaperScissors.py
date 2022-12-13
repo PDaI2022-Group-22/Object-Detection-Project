99726c7887d0ecfa370f4e8645e89399255c5191
Pelkka <79052276+Pelkka@users.noreply.github.com>
2022-12-13 19:05:21 +0200

    Functions
    Made so main.py calls modules as functions

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -4,83 +4,88 @@ import cvzone
 from cvzone.HandTrackingModule import HandDetector
 import time
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
+def Scissor():
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
 
-detector = HandDetector(maxHands=1)
+    detector = HandDetector(maxHands=1)
 
-timer = 0
-stateResult = False
-startGame = False
-scores = [0, 0]  # [AI, Player]
-
-
-while True:
-    imgBG = cv2.imread("Resources/BG.png")
-    success, img = cap.read()
-
-    imgScaled = cv2.resize(img,(0,0),None,0.64583,0.64583)
-    imgScaled = imgScaled[:,58:355]
-
-    # Find Hands
-    hands, img = detector.findHands(imgScaled)  # with draw
-
-    if startGame:
-
-        if stateResult is False:
-            timer = time.time() - initialTime
-            cv2.putText(imgBG, str(int(timer)), (446,335), cv2.FONT_HERSHEY_PLAIN, 6, (255, 0, 255), 4)
-
-            if timer>3:
-                stateResult = True
-                timer = 0
-
-                if hands:
-                    hand = hands[0]
-                    fingers = detector.fingersUp(hand)
-                    print(fingers)
-                    if fingers == [0,0,0,0,0]:
-                        playerMove = 1
-                    if fingers == [1,1,1,1,1]:
-                        playerMove = 2
-                    if fingers == [0,1,1,0,0]:
-                        playerMove = 3
-
-                    randomNumber = random.randint(1,3)
-                    imgAI = cv2.imread(f'Resources/{randomNumber}.png',cv2.IMREAD_UNCHANGED)
-                    imgBG = cvzone.overlayPNG(imgBG, imgAI, (70, 175))
-
-                    # Player wins
-                    if (playerMove == 1 and randomNumber == 3) or \
-                        (playerMove == 2 and randomNumber == 1) or \
-                        (playerMove == 3 and randomNumber == 2):
-                        scores[1] += 1
-
-                    # AI wins
-                    if (playerMove == 1 and randomNumber == 2) or \
-                        (playerMove == 2 and randomNumber == 3) or \
-                        (playerMove == 3 and randomNumber == 1):
-                        scores[0] += 1
-
-                    print(playerMove)
+    timer = 0
+    stateResult = False
+    startGame = False
+    scores = [0, 0]  # [AI, Player]
 
 
-    imgBG[176:486,594:891] = imgScaled
+    while True:
+        imgBG = cv2.imread("Resources/BG.png")
+        success, img = cap.read()
 
-    if stateResult:
-        imgBG = cvzone.overlayPNG(imgBG, imgAI, (70, 175))
+        imgScaled = cv2.resize(img,(0,0),None,0.64583,0.64583)
+        imgScaled = imgScaled[:,58:355]
 
-    cv2.putText(imgBG, str(scores[0]), (307, 161), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 4)
-    cv2.putText(imgBG, str(scores[1]), (833, 161), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 4)
+        # Find Hands
+        hands, img = detector.findHands(imgScaled)  # with draw
 
-    # cv2.imshow("Image",img)
-    cv2.imshow("BG",imgBG)
-    # cv2.imshow("Scaled",imgScaled)
+        if startGame:
+
+            if stateResult is False:
+                timer = time.time() - initialTime
+                cv2.putText(imgBG, str(int(timer)), (446,335), cv2.FONT_HERSHEY_PLAIN, 6, (255, 0, 255), 4)
+
+                if timer>3:
+                    stateResult = True
+                    timer = 0
+
+                    if hands:
+                        hand = hands[0]
+                        fingers = detector.fingersUp(hand)
+                        print(fingers)
+                        if fingers == [0,0,0,0,0]:
+                            playerMove = 1
+                        if fingers == [1,1,1,1,1]:
+                            playerMove = 2
+                        if fingers == [0,1,1,0,0]:
+                            playerMove = 3
+
+                        randomNumber = random.randint(1,3)
+                        imgAI = cv2.imread(f'Resources/{randomNumber}.png',cv2.IMREAD_UNCHANGED)
+                        imgBG = cvzone.overlayPNG(imgBG, imgAI, (70, 175))
+
+                        # Player wins
+                        if (playerMove == 1 and randomNumber == 3) or \
+                            (playerMove == 2 and randomNumber == 1) or \
+                            (playerMove == 3 and randomNumber == 2):
+                            scores[1] += 1
+
+                        # AI wins
+                        if (playerMove == 1 and randomNumber == 2) or \
+                            (playerMove == 2 and randomNumber == 3) or \
+                            (playerMove == 3 and randomNumber == 1):
+                            scores[0] += 1
+
+                        print(playerMove)
 
 
-    key = cv2.waitKey(1)
-    if key == ord('s'):
-        startGame = True
-        initialTime = time.time()
-        stateResult = False
+        imgBG[176:486,594:891] = imgScaled
+
+        if stateResult:
+            imgBG = cvzone.overlayPNG(imgBG, imgAI, (70, 175))
+
+        cv2.putText(imgBG, str(scores[0]), (307, 161), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 4)
+        cv2.putText(imgBG, str(scores[1]), (833, 161), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 4)
+
+        # cv2.imshow("Image",img)
+        cv2.imshow("BG",imgBG)
+        # cv2.imshow("Scaled",imgScaled)
+
+        key = cv2.waitKey(1)
+        if key == ord('s'):
+            startGame = True
+            initialTime = time.time()
+            stateResult = False
+
+        if key == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,28 +1,36 @@
+from RockPaperScissors import Scissor
+from EmotionCheck import FaceCheck
+from TasteOfGame import TasteRain
+from SnakeGame import AAAAAA
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 #import TasteOfGame
+from kivy.uix.screenmanager import ScreenManager, Screen
+import PeopleCounting
+import PeopleCountingVideo
 
 
-
-class GridLayoutExample(GridLayout):
-     pass
- 
-
-
-class BoxLayoutExample(BoxLayout):
-    def DD(self):
-       exec(open('TasteOfGame.py').read())
-    def EE(self):
-        exec(open('EmotionCheck.py').read())
+class MainMenu(Screen):
     def AA(self):
-        exec(open('RockPaperScissors.py').read())
-    pass
+       Scissor()
+    def DD(self):
+        TasteRain()
+    def EE(self):
+        FaceCheck()
+    def FF(self):
+        AAAAAA()
+
+
+
+class Manager(ScreenManager):
+    main = ObjectProperty(None)
+    people_counting = ObjectProperty(None)
+    people_counting_video = ObjectProperty(None)
 
 
 class ObjectDetectionApp(App):
-    
-    pass
-
+    def build(self):
+        return Manager()
 
 ObjectDetectionApp().run()
